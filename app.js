e58d66b350825f80a0540a531b6a12b8ccc2a1e9
DeOnna <36292560+DeOnna05@users.noreply.github.com>
2018-09-21 20:01:22 -0500

    view and add functions added

---
+++ app.js
@@ -0,0 +1,42 @@
+//for loop to display all employees from the array in employeeList.js
+const showAll = function(){  
+  $('.content').empty();
+
+for(let i = 0; i < employeeList.length; i++){
+    $('.content').append(`<p>${employeeList[i].name}</p>`)
+    $('.content').append(`<p>${employeeList[i].officeNum}</p>`);
+    $('.content').append(`<p>${employeeList[i].phoneNum}</p>`);
+  }
+}
+
+$('#view').on("click", showAll); 
+
+//user can add new input name, office number, and phone number and returns the updated employee list - needs a input box to enter new information
+
+
+const newInfo = function(){
+    //variables for new values to be held
+    const newName = $('#name').val();
+    const newOffice = $('#office').val(); //getting pushed as string instead of number
+    const newPhone = $('#phone').val();
+    //pushing new values to list in an object
+    employeeList.push({
+        name: newName, 
+        officeNum: newOffice, 
+        phoneNum: newPhone
+    });
+    //clear input fields and return updated list
+    $('#name').val(''); 
+    $('#office').val(''); 
+    $('#phone').val('');
+}
+
+$('#add').on("click", newInfo); 
+
+
+$('#verify').on("click", ) //after user inputs a name, it returns yes if the employee exists and no if not. (if/else statement)
+
+$('#update').on("click", ) //user inputs existing name, office number and phone number and updates the office number and phone number then returns updated employee list 
+
+
+$('#delete').on("click", ) //slice/splice
